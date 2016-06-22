08bd8cc6aeb879921711db7890abed20d1f89421
Kenny Phu <email": null>
2016-06-22 17:07:50 -0400

    update session controllers for login and logout

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -3,7 +3,14 @@ get '/session/login' do
 end
 
 post '/session' do
-
+  user = User.find_by(display_name: params[:user][:display_name])
+  if user.authenticate(params[:user][:password])
+    session['user_id'] = user.id
+    redirect '/posts'
+  # else
+  #   errors = ["Display Name or Password invalid"]
+  #   erb :'/session/login'
+  end
 end
 
 get '/session/logout' do
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,7 +1,7 @@
 def logged_in?
-  User.exists?(id: session['id'])
+  User.exists?(id: session['user_id'])
 end
 
 def current_user
-  User.find(session['id'])
+  User.find(session['user_id'])
 end
