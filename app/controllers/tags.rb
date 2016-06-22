ce61de533d0ef5d1a0a39e5a887b2b38a53b37a3
Aleksander Turberville <email": null>
2016-06-22 17:56:48 -0400

    add routes for posts and tags

--- app/controllers/tags.rb
+++ app/controllers/tags.rb
@@ -1,3 +1,18 @@
-get '/tags' do
-  redirect '/posts'
+get '/tags/new' do
+  erb :'/tags/new'
+end
+
+post '/tags' do
+  post = Post.new(params[:post])
+  if post.save
+    redirect '/tags/:id'
+  else
+    @errors = post.errors.full_messages
+    erb :'/tags/new'
+  end
+end
+
+get '/tags/:id' do
+  @tags = Tag.find(params[:id])
+  erb :'/tags/show'
 end
