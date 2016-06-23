97db6aba1457bd3fd09624a0652b7956bf25185d
Kenny Phu <email": null>
2016-06-22 20:49:54 -0400

    update answer controller to save an answer and throw errors if any are present

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,5 +1,5 @@
 post '/answers' do
-  post = posts.find(params[:post_id])
+  post = Post.find(params[:post_id])
   answer = post.answers.new(body:params[:body], user_id:current_user.id)
   if answer.save
     redirect "/posts/#{params[:post_id]}"
