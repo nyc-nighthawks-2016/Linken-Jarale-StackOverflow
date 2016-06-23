e495c69548c07e0dda5cef2b01bd09134826dfe4
Kenny Phu <email": null>
2016-06-22 20:37:17 -0400

    create a post route for creating an answer to a post

---
+++ app/controllers/answers.rb
@@ -0,0 +1,10 @@
+post '/answers' do
+  post = posts.find(params[:post_id])
+  answer = post.answers.new(body:params[:body], user_id:current_user.id)
+  if answer.save
+    redirect "/posts/#{params[:post_id]}"
+  else
+    @errors = answer.errors.full_messages
+    erb :'posts/show'
+  end
+end
