a29d901f4304e3d158d6db536f614f9ed0fdf589
Jared <email": null>
2016-06-24 14:56:35 -0400

    fixed partial layout issue

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,21 +1,16 @@
 post '/answers' do
-  if request.xhr?
-    @post = Post.find(request[:post_id])
-    answer = @post.answers.new(body:request[:body], user_id:current_user.id)
-    if answer.save
-      erb :'/partials/_answer',layout: false, locals: {answer: answer, post: @post}
+  # binding.pry
+  @post = Post.find(params[:post_id])
+  @answer = @post.answers.new(body:params[:body], user_id:current_user.id)
+
+  if @answer.save
+    if request.xhr?
+      erb :'/partials/_answer',layout: false, locals: {answer: @answer, post: @post}
     else
-      status 422
+      redirect "/posts/#{params[:post_id]}"
     end
   else
-    @post = Post.find(params[:post_id])
-    answer = @post.answers.new(body:params[:body], user_id:current_user.id)
-    if answer.save
-      redirect "/posts/#{params[:post_id]}"
-    else
-      @errors = answer.errors.full_messages
-      erb :'posts/show'
-    end
-
+    @errors = answer.errors.full_messages
+    erb :'posts/show'
   end
 end
